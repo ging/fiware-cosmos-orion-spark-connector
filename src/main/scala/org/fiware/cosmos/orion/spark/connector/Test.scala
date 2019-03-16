ca4bd4aa064ed97ad04558bbb8a84611b7e3d453
César Valencia Blancas <cesar.valencia.blancas@gmail.com>
2019-03-16 22:51:52 +0100

    Add classes

--- src/main/scala/org/fiware/cosmos/orion/spark/connector/Test.scala
+++ src/main/scala/org/fiware/cosmos/orion/spark/connector/Test.scala
@@ -6,6 +6,7 @@ import java.nio.charset.StandardCharsets
 import scala.collection.Map
 
 import org.apache.spark.SparkConf
+import org.apache.spark.internal.Logging
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.receiver.Receiver
@@ -14,15 +15,16 @@ import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.sql._
+
 object Test {
 
   def main(args: Array[String]): Unit = {
-
+    print("yes")
     val sparkConf = new SparkConf().setAppName("CustomReceiver").setMaster("local")
     val ssc = new StreamingContext(sparkConf, Seconds(1))
-    val customReceiverStream = ssc.receiverStream(new OrionReceiver("138.4.7.110", 9001))
+    val customReceiverStream = ssc.receiverStream(new OrionReceiver("localhost", 9001))//host ="138.4.7.110"
+
     customReceiverStream.print()
-    print("yes")
     ssc.start()
     ssc.awaitTermination()
   }
