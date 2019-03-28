662e7f04c1f6d4ab13df5f25e5502f4448909d71
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2019-03-28 18:33:48 +0100

    Fix Orion Sink

--- src/main/scala/org/fiware/cosmos/orion/spark/connector/Test.scala
+++ src/main/scala/org/fiware/cosmos/orion/spark/connector/Test.scala
@@ -1,35 +1,25 @@
 package org.fiware.cosmos.orion.spark.connector
 
-import java.io.{BufferedReader, InputStreamReader}
-import java.net.Socket
-import java.nio.charset.StandardCharsets
-import scala.collection.Map
-
 import org.apache.spark.SparkConf
-import org.apache.spark.internal.Logging
-import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.spark.streaming.receiver.Receiver
-
-import org.apache.spark._
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._
-import org.apache.spark.sql._
+import org.apache.spark.streaming.dstream.DStream
 
 object Test {
 
   def main(args: Array[String]): Unit = {
     print("yes")
-    val sparkConf = new SparkConf().setAppName("CustomReceiver").setMaster("local[5]")
+    val sparkConf = new SparkConf().setAppName("CustomReceiver").setMaster("local[3]")
     val ssc = new StreamingContext(sparkConf, Seconds(1))
-    val customReceiverStream = ssc.receiverStream(new OrionReceiver("localhost", 9001))//host ="138.4.7.110"
+    val customReceiverStream : DStream[NgsiEvent] = ssc.receiverStream(new OrionReceiver("localhost", 9001))//host ="138.4.7.110"
     print("---------------------------------------------------")
     customReceiverStream.print
 
-
-//    customReceiverStream.foreachRDD(a=>a.foreach(b=>OrionSender.process( OrionSenderObject("", "",ContentType.Plain, HTTPMethod.PATCH)))
-   // )
-
+    val res : DStream[OrionSinkObject] = customReceiverStream
+      .flatMap(event => event.entities)
+      .map(s =>{
+        OrionSinkObject("{'msg': 'hola'}","http://localhost:5000/fiware",ContentType.JSON, HTTPMethod.POST)
+      })
+    OrionSink.addSink(res)
 
     ssc.start()
 
