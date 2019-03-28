75cc65a8f11bd3ebb685f8e27025cae86649face
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2019-03-28 18:33:48 +0100

    Fix Orion Sink

--- src/main/scala/org/fiware/cosmos/orion/spark/connector/OrionReceiver.scala
+++ src/main/scala/org/fiware/cosmos/orion/spark/connector/OrionReceiver.scala
@@ -1,14 +1,7 @@
 package org.fiware.cosmos.orion.spark.connector
 
-import java.io.{BufferedReader, InputStreamReader}
-import java.net.Socket
-import java.nio.ByteBuffer
-import java.nio.charset.StandardCharsets
-
-import org.apache.spark.SparkConf
 import org.apache.spark.internal.Logging
 import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.receiver.Receiver
 
 
@@ -18,17 +11,11 @@ class OrionReceiver(host: String, port: Int)
 
 private var server: OrionHttpServer =_
 
-  def receive(x: NgsiEvent): Unit ={
-    store(x)
-  }
-
   def onStart() = {
-    server = new OrionHttpServer(receive)
+    server = new OrionHttpServer(store)
     server.start(port, None)
   }
 
-  // There is nothing much to do as the thread calling receive()
-  // is designed to stop by itself if isStopped() returns false
   def onStop(): Unit = {
     server.close()
   }
--- src/main/scala/org/fiware/cosmos/orion/spark/connector/OrionSink.scala
+++ src/main/scala/org/fiware/cosmos/orion/spark/connector/OrionSink.scala
@@ -1,110 +1,73 @@
-//package org.fiware.cosmos.orion.spark.connector
-//
-//import org.apache.spark.sql.{ForeachWriter, Row}
-//import org.apache.http.client.methods.HttpPatch
-//import org.apache.http.client.methods.HttpPut
-//import org.apache.http.client.methods.HttpPost
-//import org.apache.http.client.methods.HttpEntityEnclosingRequestBase
-//
-//import org.apache.http.entity.StringEntity
-//import org.apache.http.impl.client.HttpClientBuilder
-//import org.slf4j.LoggerFactory
-//
-///**
-//  * Message type accepted by the Orion Sink
-//  * @author @sonsoleslp
-//  * @param content Content of the body of the message
-//  * @param url URL to which the message will be sent
-//  * @param contentType Type of content. It can be: ContentType.JSON or ContentType.Plain
-//  * @param method HTTP Method. It can be: HTTPMethod.POST, HTTPMethod.PUT, HTTPMethod.PATCH
-//  */
-//case class OrionSenderObject(content: String, url: String, contentType: ContentType.Value, method: HTTPMethod.Value)
-//
-///**
-//  * Content type of the HTTP message
-//  */
-//object ContentType extends Enumeration {
-//  type ContentType = Value
-//  val JSON = Value("application/json")
-//  val Plain = Value("text/plain")
-//}
-//
-///**
-//  * HTTP Method of the message
-//  */
-//object HTTPMethod extends Enumeration {
-//  type HTTPMethod = Value
-//  val POST = Value("HttpPost")
-//  val PUT = Value("HttpPut")
-//  val PATCH = Value("HttpPatch")
-//}
-//
-///**
-//  * Sink for sending Flink processed data to the Orion Context Broker
-//  */
-//class OrionSender extends ForeachWriter
-//
-///**
-//  * Singleton instance of OrionSink
-//  */
-//object OrionSink {
-//
-//  /**
-//    * Function for adding the Orion Sink
-//    * @param stream DataStream of the OrionSinkObject
-//    */
-//  def addSink( datasetOfString: DataStream[OrionSenderObject]): Unit  = {
-//
-//    datasetOfString.writeStream.foreach(
-//      new ForeachWriter[String] {
-//
-//        def open(partitionId: Long, version: Long): Boolean = {
-//          // Open connection
-//
-//        }
-//
-//        def process(record: String) = {
-//          // Write string to connection
-//          val httpEntity : HttpEntityEnclosingRequestBase= createHttpMsg(msg)
-//          val client = HttpClientBuilder.create.build
-//          val response = client.execute(httpEntity)
-//        }
-//
-//        def close(errorOrNull: Throwable): Unit = {
-//          // Close the connection
-//          logger.error(errorOrNull.toString)
-//        }
-//      }
-//    ).start()
-//
-//    stream.addSink( msg => {
-//
-//      val httpEntity : HttpEntityEnclosingRequestBase= createHttpMsg(msg)
-//
-//      val client = HttpClientBuilder.create.build
-//
-//      try {
-//        val response = client.execute(httpEntity)
-//        logger.info("POST to " + msg.url)
-//      } catch {
-//        case e: Exception => {
-//          logger.error(e.toString)
-//        }
-//      }
-//
-//    })
-//
-//  }
-//
-//  /**
-//    * Create the HTTP message from the specified params
-//    * @param msg OrionSinkObject
-//    * @return Built Http Entity
-//    */
-//  def createHttpMsg(msg: OrionSender) : HttpEntityEnclosingRequestBase= {
-//    val httpEntity = getMethod(msg.method, msg.url)
-//    httpEntity.setHeader("Content-type", msg.contentType.toString)
-//    httpEntity.setEntity(new StringEntity((msg.content)))
-//    httpEntity
-//  }
-//}
+package org.fiware.cosmos.orion.spark.connector
+
+import org.apache.http.client.methods.{HttpEntityEnclosingRequestBase, HttpPatch, HttpPost, HttpPut}
+import org.apache.http.entity.StringEntity
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.dstream.DStream
+import org.slf4j.LoggerFactory
+
+case class OrionSinkObject(content: String, url: String, contentType: ContentType.Value, method: HTTPMethod.Value)
+
+object ContentType extends Enumeration {
+  type ContentType = Value
+  val JSON = Value("application/json")
+  val Plain = Value("text/plain")
+}
+
+/**
+  * HTTP Method of the message
+  */
+object HTTPMethod extends Enumeration {
+  type HTTPMethod = Value
+  val POST = Value("HttpPost")
+  val PUT = Value("HttpPut")
+  val PATCH = Value("HttpPatch")
+}
+
+
+object OrionSink {
+  private lazy val logger = LoggerFactory.getLogger(getClass)
+
+  def addSink(rddStream: DStream[OrionSinkObject]): Unit ={
+    rddStream.foreachRDD(_.foreach(process))
+  }
+
+  private def process(msg: OrionSinkObject): Unit = {
+
+    val httpEntity : HttpEntityEnclosingRequestBase= createHttpMsg(msg)
+
+    val client = HttpClientBuilder.create.build
+
+    try {
+      val response = client.execute(httpEntity)
+      logger.info("POST to " + msg.url)
+    } catch {
+      case e: Exception => {
+        logger.error(e.toString)
+      }
+    }
+
+  }
+
+  /**
+    * Http object creator for sending the message
+    * @param method HTTP Method
+    * @param url Destination URL
+    * @return HTTP object
+    */
+  def getMethod(method: HTTPMethod.Value, url: String): HttpEntityEnclosingRequestBase = {
+    method match {
+      case HTTPMethod.POST => new HttpPost(url)
+      case HTTPMethod.PUT => new HttpPut(url)
+      case HTTPMethod.PATCH => new HttpPatch(url)
+    }
+  }
+
+  def createHttpMsg(msg: OrionSinkObject) : HttpEntityEnclosingRequestBase= {
+    val httpEntity = getMethod(msg.method, msg.url)
+    httpEntity.setHeader("Content-type", msg.contentType.toString)
+    httpEntity.setEntity(new StringEntity(msg.content))
+    httpEntity
+  }
+}
